02be8607e5e02050f07dc846d1861aac8c49f8be
arshithaManaf7034 <arshithamanaf15@gmail.com>
2025-12-26 10:48:04 +0530

    Create Favourite_Singer.py

---
+++ python-practice/Favourite_Singer.py
@@ -0,0 +1,19 @@
+python-practice/
+│
+├── basics/
+│   ├── Favourite_Singer.py
+│   ├── Maximum_borders.p
+│   ├── Number_of_steps.py
+│   ├── Zoos.py
+│   ├── Split_houses.py
+│   ├── loops.py
+│
+├── dsa/
+│   ├── 
+│   ├── 
+│
+├── hacker_earth/
+│   ├── 
+│   ├── 
+│
+└── README.md
