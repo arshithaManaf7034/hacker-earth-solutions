7521652c7230da741f560abb009cbcc66e900f0f
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-09 23:27:08 +0530

    Create roy_and_profilepic.py

---
+++ python-practice/roy_and_profilepic.py
@@ -0,0 +1,12 @@
+L = int(input())
+N = int(input())
+
+for _ in range(N):
+    W, H = map(int, input().split())
+
+    if W < L or H < L:
+        print("UPLOAD ANOTHER")
+    elif W == H:
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
